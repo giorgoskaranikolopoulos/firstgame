7da69dfc4060e563022c4c54fecf375bf8e8871e
giorgoskaranikolopoulos <giorgoskaranikol@gmail.com>
2023-10-12 23:40:03 +0300

    constructor stage 1

--- code.cpp
+++ code.cpp
@@ -6,7 +6,7 @@
 #define screenHeight 450
 
 // Syntax for creating a class
-class rectangle { // Ολες οι κλάσεις κατα σύμβαση ξεκινάνε με κεφαλαίο γράμμα. Αντί για rectangle θα έλεγα Rectangle.
+class MyRectangle { // Ολες οι κλάσεις κατα σύμβαση ξεκινάνε με κεφαλαίο γράμμα. Αντί για rectangle θα έλεγα MyRectangle.
     // Access specifier
 public:
     // Member variables (also known as data members)
@@ -17,34 +17,40 @@ public:
     int speedx,speedy;
     Color color;
     */
-    int x;
-    int y; 
+    int x,y; 
     Color color;
-    int width;
-    int height;
-    int speedx;
-    int speedy;
+    int width,height;
+    int speedx,speedy;
+
 
     // Member functions (methods)
-
+    MyRectangle(int x,int y,Color color,int height,int width,int speedx,int speedy) {
+        this->x=x;
+        this->y=y;
+        this->color=color;
+        this->height=height; 
+        this->width=width;
+        this->speedx=speedx;
+        this->speedy=speedy;  
+    }
     Color changeColor (int i) {
         // Function implementation
-        int metro; // ΤΟ Μετρό της Θεσσαλονικης?
-        Color matrix[3]; // καλύτερο όνομα: colors. matrix είναι πολύ γενικό.
+        int rectanglecolor; // ΤΟ Μετρό της Θεσσαλονικης?
+        Color colors[3]; // καλύτερο όνομα: colors. matrix είναι πολύ γενικό.
 
-        matrix[0]=GREEN;
-        matrix[1]=BLUE;
-        matrix[2]=RED;
+        colors[0]=GREEN;
+        colors[1]=BLUE;
+        colors[2]=RED;
 
-        metro=i%3;
+        rectanglecolor=i%3;
 
-        return matrix[metro];
+        return colors[rectanglecolor];
     }
 
     // τι είναι το price? τι είναι το sizeR και το sizeP? βάλε καλύτερα ονόματα.
-    bool hitWall(int price,int step,int sizeR,int sizeP) {
+    bool hitWallOR() {
         // από ότι βλέπω μπορείς δεν χρειάζεσαι μόνο το step σαν παράμετρο.
-        if((price+sizeR>=sizeP&&step>0)||(price<=0&&step<0)) { // γιατί σε ενδιαφέρει το πρόσημο του step?
+        if((this->x+this->width>=screenWidth)||(this->x<=0)) { // γιατί σε ενδιαφέρει το πρόσημο του step?
             return true;
         }
         else {
@@ -57,35 +63,25 @@ public:
 int main(void)
 {
     // Initialization
-    int stepX,step2X,stepY,step2Y,metro,i1,i2;
+    int stepX,step2X,stepY,step2Y,i1,i2;
     Color matrix[3];
-    rectangle rectangle1,rectangle2;
+    MyRectangle rectangle1( 0,0,RED,50,50,80,80);
+    MyRectangle rectangle2( 0,400,GREEN,50,50,80,80);
     clock_t start_time,end_time; 
     double elapsed_time,speedX,speedY,speed2X,speed2Y,loop_speedX,loop_speed2X,loop_speedY,loop_speed2Y;
     InitWindow(screenWidth,screenHeight, "raylib [core] example - basic window");
 
 
     // CONSTUCTOR για να μην έχεις αυτό το μακρινάρι που έχεις παρακάτω.
-    rectangle1.x=0;
-    rectangle1.y=0;    
-    rectangle2.x=0;
-    rectangle2.y=400;
+
     stepX=0;
     stepY=0;
     step2X=0;
     step2Y=0;
-    rectangle1.speedx=80;
-    rectangle1.speedy=80;
-    rectangle2.speedx=80;
-    rectangle2.speedy=80;
     loop_speedX=0;
     loop_speedY=0;
     loop_speed2X=0;
     loop_speed2Y=0;
-    rectangle1.height=50;
-    rectangle1.width=50;
-    rectangle2.height=50;
-    rectangle2.width=50;
     i1=3;
     i2=3;
      
@@ -113,19 +109,19 @@ int main(void)
         rectangle2.x=rectangle2.x+step2X;
         rectangle2.y=rectangle2.y+step2Y;
         
-        if(rectangle1.hitWall(rectangle1.x,stepX,rectangle1.width,screenWidth)) {
+        if(rectangle1.hitWallOR()&&stepX!=0) {
             i1++;
             rectangle1.speedx=-rectangle1.speedx; // Κάνε μια συνάρτηση moveRectangle.
         }
-        if(rectangle1.hitWall(rectangle1.y,stepY,rectangle1.height,screenHeight)) {
+        if(rectangle1.hitWallOR()&&stepY!=0) {
             i1++;
             rectangle1.speedy=-rectangle1.speedy;
         }
-        if(rectangle2.hitWall(rectangle2.x,step2X,rectangle2.width,screenWidth)) {  
+        if(rectangle2.hitWallOR()&&step2X!=0) {  
             rectangle2.speedx=-rectangle2.speedx;
             i2++;
         }
-        if(rectangle2.hitWall(rectangle2.y,step2Y,rectangle2.height,screenHeight)) { 
+        if(rectangle2.hitWallOR()&&step2Y!=0) { 
             rectangle2.speedy=-rectangle1.speedy;           
             i2++;
         }
